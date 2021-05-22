827269019a77bbe0f4aa807895de632e8f1b257a
yourlocalbusinessstores <yourlocalbusinessstores@gmail.com>
2021-05-22 16:46:13 +0100

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 import './style.css';
 import SignInSide from './signinslide';
+import { Route, Switch } from 'react-router-dom';
+import Home from './home';
 
 export default function App() {
   return (
-    <div>
-      <SignInSide />
-    </div>
+    <Fragment>
+      <div />
+      <Switch>
+        <Route exact path="/">
+          <SignInSide />
+        </Route>
+        <Route exact path="/login">
+          <SignInSide />
+        </Route>
+        <Route exact path="/home">
+          <Home />
+        </Route>
+      </Switch>
+    </Fragment>
   );
 }
