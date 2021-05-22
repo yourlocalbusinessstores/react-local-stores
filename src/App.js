0665f57bb0ed8428b42ff7f5f31ce0c558cd765f
yourlocalbusinessstores <yourlocalbusinessstores@gmail.com>
2021-05-22 16:38:19 +0100

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './style.css';
+import SignInSide from './signinslide';
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <SignInSide />
     </div>
   );
 }
