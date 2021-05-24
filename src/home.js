3e6fa6020295799741f32ee025a48b6115e0477f
yourlocalbusinessstores <yourlocalbusinessstores@gmail.com>
2021-05-24 17:21:14 +0100

    1 file modified

--- src/home.js
+++ src/home.js
@@ -13,20 +13,8 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
+import Copyright from './Copyright';
 import { DropzoneArea } from 'material-ui-dropzone';
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
 
 const useStyles = makeStyles(theme => ({
   icon: {
