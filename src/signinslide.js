b640d3eb903b5ac940c6abda39f4c84bb29c507a
yourlocalbusinessstores <yourlocalbusinessstores@gmail.com>
2021-05-24 17:20:43 +0100

    1 file modified, 1 added

--- src/signinslide.js
+++ src/signinslide.js
@@ -13,19 +13,7 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import { useHistory } from 'react-router-dom';
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+import Copyright from './Copyright';
 
 const useStyles = makeStyles(theme => ({
   root: {
